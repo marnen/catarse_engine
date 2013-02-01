70e4efc9dd8c0f216afde21f5acd1d1155325db0
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-02-01 12:49:25 -0200

    Adds dummy app for specs

---
+++ core/spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '3cc772eba49afc06bddff754e41e59545286c852e5bbe5aa842c460b7738b4701c5aecf4859bd86d8b64b5b7ebece379a351d02f2da2ff6cd2a1cf53e42cba0a'
--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
+require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'sidekiq/testing'
 
@@ -27,17 +27,17 @@ RSpec.configure do |config|
   config.before(:each) do
     DatabaseCleaner.start
     ActionMailer::Base.deliveries.clear
-    Project.any_instance.stubs(:store_image_url).returns('http://www.store_image_url.com')
-    Project.any_instance.stubs(:download_video_thumbnail)
+    Catarse::Project.any_instance.stubs(:store_image_url).returns('http://www.store_image_url.com')
+    Catarse::Project.any_instance.stubs(:download_video_thumbnail)
     CatarseMailchimp::API.stubs(:subscribe)
     CatarseMailchimp::API.stubs(:unsubscribe)
-    Notification.stubs(:create_notification)
-    Notification.stubs(:create_notification_once)
-    ProjectObserver.any_instance.stubs(:after_create)
-    Calendar.any_instance.stubs(:fetch_events_from)
-    Blog.stubs(:fetch_last_posts).returns([])
-    ProjectsController.any_instance.stubs(:last_tweets)
-    [Projects::BackersController, ::BackersController, UsersController, UnsubscribesController, ProjectsController, ExploreController, SessionsController].each do |c|
+    Catarse::Notification.stubs(:create_notification)
+    Catarse::Notification.stubs(:create_notification_once)
+    Catarse::ProjectObserver.any_instance.stubs(:after_create)
+    Catarse::Calendar.any_instance.stubs(:fetch_events_from)
+    Catarse::Blog.stubs(:fetch_last_posts).returns([])
+    Catarse::ProjectsController.any_instance.stubs(:last_tweets)
+    [Catarse::Projects::BackersController, Catarse::BackersController, Catarse::UsersController, Catarse::UnsubscribesController, Catarse::ProjectsController, Catarse::ExploreController, Catarse::SessionsController].each do |c|
       c.any_instance.stubs(:render_facebook_sdk)
       c.any_instance.stubs(:render_facebook_like)
       c.any_instance.stubs(:render_twitter)
