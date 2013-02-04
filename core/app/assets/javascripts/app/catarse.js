32cf9c8964873e1f2ea1e075a7180c9f2298b6f6
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-02-04 18:56:38 -0200

    Adjusts the namespaces on some javascripts

--- core/app/assets/javascripts/app/catarse.js
+++ core/app/assets/javascripts/app/catarse.js
@@ -35,66 +35,68 @@ var CATARSE = {
         Backbone.history.start()
     }
   },
-  explore:{
-    index: function(){
-       window.view = new CATARSE.ExploreIndexView({el: $("body") });
-    }
-  },
-  adm: {
-    users:{
+  catarse: {
+    explore:{
       index: function(){
-        window.view = new CATARSE.Adm.Users.Index({el: $("body") });
+         window.view = new CATARSE.ExploreIndexView({el: $("body") });
       }
     },
-
-    backers:{
-      index: function(){
-        window.view = new CATARSE.Adm.Backers.Index({el: $("body") });
-      }
-    }
-  },
-  projects: {
-    index: function(){
-      window.view = new CATARSE.ProjectsIndexView({el: $("body") });
-    },
-    show: function(){
-      window.view = new CATARSE.ProjectsShowView({el: $("body") });
-    },
-    embed: function(){
-      window.view = new CATARSE.ProjectsEmbedView({el: $("body") });
-    },
-    video_embed: function(){
-      window.view = new CATARSE.ProjectsVideo_embedView({el: $("body") });
-    },
-    'new': function(){
-      return new CATARSE.ProjectsNewView();
-    },
-    pending: function(){
-      window.view = new CATARSE.ProjectsPendingView({el: $("body") });
-    },
-    pending_backers: function(){
-      window.view = new CATARSE.ProjectsPending_backersView({el: $("body") });
-    },
-    start: function(){
-      window.view = new CATARSE.ProjectsStartView({el: $("body") });
-    },
-    backers: {
-      'new': function(){
-        window.view = new CATARSE.BackersNewView({el: $("body") });
+    adm: {
+      users:{
+        index: function(){
+          window.view = new CATARSE.Adm.Users.Index({el: $("body") });
+        }
       },
-      review: function(){
-        window.view = new CATARSE.BackersReviewView({el: $("body") });
+
+      backers:{
+        index: function(){
+          window.view = new CATARSE.Adm.Backers.Index({el: $("body") });
+        }
       }
-    }
-  },
-  static: {
-    guidelines: function(){
-      window.view = new CATARSE.StaticGuidelinesView({el: $("body") });
-    }
-  },
-  users: {
-    show: function(){
-      window.view = new CATARSE.UsersShowView({el: $("body") });
+    },
+    projects: {
+      index: function(){
+        window.view = new CATARSE.ProjectsIndexView({el: $("body") });
+      },
+      show: function(){
+        window.view = new CATARSE.ProjectsShowView({el: $("body") });
+      },
+      embed: function(){
+        window.view = new CATARSE.ProjectsEmbedView({el: $("body") });
+      },
+      video_embed: function(){
+        window.view = new CATARSE.ProjectsVideo_embedView({el: $("body") });
+      },
+      'new': function(){
+        return new CATARSE.ProjectsNewView();
+      },
+      pending: function(){
+        window.view = new CATARSE.ProjectsPendingView({el: $("body") });
+      },
+      pending_backers: function(){
+        window.view = new CATARSE.ProjectsPending_backersView({el: $("body") });
+      },
+      start: function(){
+        window.view = new CATARSE.ProjectsStartView({el: $("body") });
+      },
+      backers: {
+        'new': function(){
+          window.view = new CATARSE.BackersNewView({el: $("body") });
+        },
+        review: function(){
+          window.view = new CATARSE.BackersReviewView({el: $("body") });
+        }
+      }
+    },
+    static: {
+      guidelines: function(){
+        window.view = new CATARSE.StaticGuidelinesView({el: $("body") });
+      }
+    },
+    users: {
+      show: function(){
+        window.view = new CATARSE.UsersShowView({el: $("body") });
+      }
     }
   }
 }
