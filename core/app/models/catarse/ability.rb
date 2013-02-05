7721b4bafc864f4926326a591ae728c780e7a9f0
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-02-05 15:15:30 -0200

    Makes the cancan ability works
    Now works fine with Rails Engine namespaced models

--- core/app/models/catarse/ability.rb
+++ core/app/models/catarse/ability.rb
@@ -6,6 +6,11 @@ class Ability
   def initialize(current_user)
     current_user ||= User.new
 
+    alias_subject :'catarse/projects', :to => :projects
+    alias_subject :'catarse/rewards', :to => :rewards
+    alias_subject :'catarse/updates', :to => :updates
+    alias_subject :'catarse/users', :to => :users
+
     can :read, :all
 
     # NOTE: Update authorizations
